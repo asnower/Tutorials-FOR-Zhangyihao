e04f2abbfe339a8c8f324f7f15dff5a9507d3e30
asnower <49426814+asnower@users.noreply.github.com>
2020-05-14 17:05:32 +0900

    Update nnExample.py

--- nnExample.py
+++ nnExample.py
@@ -1,23 +1,21 @@
 import torch
 import random
-N, D_in, H, D_out = 10, 5, 100, 1
+N, D_in, D_out = 10, 5, 1
 
 x = torch.randn(N, D_in)
 y = torch.randn(N, D_out)
 for i in range(10):
     y[i] = random.randint(0,1)
 model = torch.nn.Sequential(
-    torch.nn.Linear(D_in, H),
-    torch.nn.ReLU(),
-    torch.nn.Linear(H, D_out),
+    torch.nn.Linear(D_in, D_out)
 )
 loss_fn = torch.nn.MSELoss(reduction='sum')
 learning_rate = 1e-4
-for t in range(500):
+for t in range(501):
     for i in range(10):
         y_pred = model(x[i])
         loss = loss_fn(y_pred, y[i])
-        if t % 100 == 99:
+        if t % 100 == 0:
             print(t, loss.item())
         model.zero_grad()
         loss.backward()
